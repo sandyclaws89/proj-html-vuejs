3e151e3e36a1984862cb15fb4bc802de4f2666e2
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-04-01 16:19:20 +0200

    creata struttura dati nav header

---
+++ src/components/FooterTeach.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterTeach',
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HeaderTeach.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HeaderTeach',
+    data() {
+        return {
+            arrHeaderNav: [
+                {
+                    categories: 'homes',
+                    subcatergories1: 'textsub1',
+                    subcatergories2: 'textsub2',
+                    subcatergories3: 'textsub3',
+                },
+                {
+                    categories: 'pages',
+                    subcatergories1: 'textsub1',
+                    subcatergories2: 'textsub2',
+                    subcatergories3: 'textsub3',
+                },
+                {
+                    categories: 'blog',
+                    subcatergories1: 'textsub1',
+                    subcatergories2: 'textsub2',
+                    subcatergories3: 'textsub3',
+                },
+                {
+                    categories: 'shop',
+                    subcatergories1: 'textsub1',
+                    subcatergories2: 'textsub2',
+                    subcatergories3: 'textsub3',
+                },
+                {
+                    categories: 'events',
+                    subcatergories1: 'textsub1',
+                    subcatergories2: 'textsub2',
+                    subcatergories3: 'textsub3',
+                },
+                {
+                    categories: 'elements',
+                    subcatergories1: 'textsub1',
+                    subcatergories2: 'textsub2',
+                    subcatergories3: 'textsub3',
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
