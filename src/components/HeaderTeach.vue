c809ba9958bb4ad5778dd440ce8771c0c3df49ff
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-04-01 17:54:32 +0200

    struttura header

--- src/components/HeaderTeach.vue
+++ src/components/HeaderTeach.vue
@@ -1,8 +1,30 @@
 <template>
+<!--TODO:FARE LO SLIDE -->
 <header class="flex">
     <nav class="flex main-container">
-
+        <img src="../assets/img/logo-img-01.png" alt="">
+        <ul>
+            <li v-for="(category, index) in arrHeaderNav" :key="index">{{category.categories}}</li>
+            <li>ICN</li> <!--TODO:CERCA SVG -->
+            <li>ICN</li> <!--TODO:CERCA SVG -->
+        </ul>
     </nav>
+    <section class="main-container flex jumbotron">
+        <!--TODO:SISTEMA BENE STILE -->
+        <span>ICONS</span>
+        <div>
+            <h1>
+                Our team
+            </h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est rerum quo nam aspernatur sit itaque odit soluta hic.
+            </p>
+            <button>READ MORE</button>
+            <button>PURCHASE</button>
+        </div>
+       
+        <span>ICONS</span>
+    </section>
 </header>
 </template>
 
@@ -13,37 +35,37 @@ export default {
         return {
             arrHeaderNav: [
                 {
-                    categories: 'homes',
+                    categories: 'HOMES',
                     subcatergories1: 'textsub1',
                     subcatergories2: 'textsub2',
                     subcatergories3: 'textsub3',
                 },
                 {
-                    categories: 'pages',
+                    categories: 'PAGES',
                     subcatergories1: 'textsub1',
                     subcatergories2: 'textsub2',
                     subcatergories3: 'textsub3',
                 },
                 {
-                    categories: 'blog',
+                    categories: 'BLOG',
                     subcatergories1: 'textsub1',
                     subcatergories2: 'textsub2',
                     subcatergories3: 'textsub3',
                 },
                 {
-                    categories: 'shop',
+                    categories: 'SHOP',
                     subcatergories1: 'textsub1',
                     subcatergories2: 'textsub2',
                     subcatergories3: 'textsub3',
                 },
                 {
-                    categories: 'events',
+                    categories: 'EVENTS',
                     subcatergories1: 'textsub1',
                     subcatergories2: 'textsub2',
                     subcatergories3: 'textsub3',
                 },
                 {
-                    categories: 'elements',
+                    categories: 'ELEMENTS',
                     subcatergories1: 'textsub1',
                     subcatergories2: 'textsub2',
                     subcatergories3: 'textsub3',
@@ -58,21 +80,47 @@ export default {
 header{
     background-image: url('../assets/img/rev-slider-main-home-img-03.jpg');
     justify-content: center;
-    width: 100%;
     height: 90vh;
     background-position: center;
+    flex-wrap: wrap;
     nav{
         height: 50px;
-        background-color: rebeccapurple;
-        justify-content: center;
+        justify-content: space-between;
+        align-items: center;
+        img{
+            height: 50%;
+        }
+        li{
+            display: inline; 
+            padding: 0 15px;
+            font-weight: 500;
+        }
     }
+
     .jumbotron{
+        justify-content: space-between;
+        align-items: flex-start;
+        div{
+            margin-left: -500px;
+            width: 400px;
+            h1{
+            font-size: 4rem;
+            font-family: serif;
+            width: 70%;
+            }
+            p{
+             padding-top: 10px;
+             width: 50%;
+            }
+            button{
+                width: 50%;
+                height: 30px;
+            }
+                
+        }  
         
-        
-        justify-content: center;
-       
-        object-fit:fill
     }
+    
 
 }
 </style>
