acdce9184d824b6f605f912ca4d0e14decb6246b
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-04-04 16:56:35 +0200

    Terminata bozza sezione events

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="card flex">
-      <div class="date">{{CardDate}}</div>
+      <div class="date flex">
+          {{CardDate[0]}}
+          <div>{{CardDate[1]}},{{CardDate[2]}}</div>
+      </div>
       <div class="flex text">
             <h3>{{CardTitle}}</h3>
-            
+            <div class="flex data-box">
+                <div class="icon">ICN</div>    
+                <div class="data">{{CardHours}}</div>
+            </div>
+            <div class="flex data-box">
+                <div class="icon">ICN</div>    
+                <div class="data">{{CardPlace}}</div>
+            </div>
+            <div>
+                READ MORE
+            </div>
       </div>
   </div>
 </template>
@@ -12,14 +25,67 @@
 export default {
     name:'EventCard',
     props: {
+        CardDate: String,
         CardTitle: String,
-
+        CardHours:String,
+        CardPlace: String,
     }
 }
 </script>
 
 <style lang="scss">
     .card{
-        background-color: rebeccapurple;
+        // background-color: rebeccapurple;
+        padding:20px;
+        border-bottom: solid #ececec ;
+        border-width: 1px;
+        .date{
+            flex-basis: 25%;
+            flex-direction: column;
+            background-color: #ff4612;
+            text-align: center;
+            padding:5px;
+            font-family: serif;
+            font-size: 1.5rem;
+            font-weight: 600;
+            color:white;
+            align-self: flex-start;
+            div{
+                font-size: 1rem;
+                font-weight: 500;
+            }
+        }
+        .text{
+            width: 70%;
+            flex-wrap: wrap;
+            align-items: center;
+            margin-left: 20px;
+           
+            h3{
+                width: 100%;
+                font-family: serif;
+                font-size: 1.5rem;
+            }
+            .data-box{
+                width: 100%;
+                align-items: center;
+                color: #8e8e8e;
+                padding-top: 5px;
+            }
+            .icon{
+                width: 10%;
+                background-color: rosybrown;
+            }
+            .data{
+                width: 90%;
+                padding: 10px 0;
+                padding-left: 8px;
+
+            }
+            >:last-child{
+               padding-top:20px;
+               font-weight: 600;
+            }
+        }
     }
 </style>
--- src/components/EventsSection.vue
+++ src/components/EventsSection.vue
@@ -1,11 +1,18 @@
 <template>
   <section class="flex">
       <div class="main-container flex">
-          <div class="events-box">
+          <div class="events-box relative">
               <h2>
                   Upcoming Events
               </h2>
-              <EventCard/>
+              <EventCard
+              v-for="(event, index) in arrEvents"
+              :key="index"
+              :CardDate="event.date"
+              :CardTitle="event.event"
+              :CardHours="event.hours"
+              :CardPlace="event.place"
+              />
           </div>
           <!-- <img src="../assets/img/h1-img-09.jpg" alt=""> -->
       </div>
@@ -18,6 +25,41 @@ export default {
     name:'EventsSection',
     components: {
         EventCard
+    },
+    data() {
+        return {
+            arrEvents: [
+                {
+                    event:'Melbourne Coaching',
+                    hours: '9:00 am - 5:00pm, jan 7, 2022',
+                    place: 'Cambridge, MA 02138, USA',
+                    date: ['07', 'jan','2022'],
+                },
+                 {
+                    event:'New York Coaching',
+                    hours: '9:00 am - 5:00pm, jan 7, 2022',
+                    place: 'Cambridge, MA 02138, USA',
+                    date: ['11', 'apr','2022'],
+                },
+                 {
+                    event:'Melbourne Coaching',
+                    hours: '9:00 am - 5:00pm, jan 7, 2022',
+                    place: 'Cambridge, MA 02138, USA',
+                    date: ['21', 'dec','2022'],
+                },
+                 {
+                    event:'Berlin Coaching',
+                    hours: '9:00 am - 5:00pm, jan 7, 2022',
+                    place: 'Cambridge, MA 02138, USA',
+                    date: ['18', 'jul','2022'],
+                },
+                 
+                
+                
+                
+
+            ],
+        }
     }
 }
 </script>
@@ -31,16 +73,24 @@ section{
         height: 100vh; 
         justify-content: center;
         align-content: center;
+        // position: relative;
+
         .main-container{
             align-items: center;
         }
          .events-box{
-            width: 300px;
-            height: 60%;
+            width: 500px;
+            height: 70%;
             background-color: white;
+            overflow-y: scroll;
+            overflow-x: hidden;
                 h2{
+                    width: 450px;
                     padding: 15px;
                     border-bottom: solid #efefef;
+                    // TODO: NON RIESCO A DARE POSITION FIXED
+                    // position: fixed;
+                    // top: 0px;
                 }
         }
 }
